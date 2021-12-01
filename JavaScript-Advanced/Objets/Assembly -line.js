1cae4238258f53c7b22cd03f27c34eaa47f92de9
DannyDoneva96 <danielasinmas1@gmail.com>
2021-12-01 14:19:18 +0200

    HTML and CSS

---
+++ JavaScript-Advanced/Objets/Assembly -line.js
@@ -0,0 +1,53 @@
+function createAssemblyLine() {
+
+    let myObj = {
+
+        hasClima: function hasClima(car) {
+            car.temp = 21;
+            car.tempSettings = 21;
+            car.adjustTemp = () => {
+                if (car.temp < car.tempSettings) {
+                    car.temp++;
+                } else if (car.temp > car.tempSettings) {
+                    car.temp++
+                }
+            }
+        },
+        hasAudio: (obj)=>{
+            obj.currentTrack = {'name': '', 'artist': ''}
+
+        },
+        hasParktronic: hasParktronic(),
+    }
+
+
+
+
+    return myObj;
+}
+const assemblyLine = createAssemblyLine();
+
+const myCar = {
+    make: 'Toyota',
+    model: 'Avensis'
+};
+
+
+assemblyLine.hasClima(myCar);
+console.log(myCar.temp);
+myCar.tempSettings = 18;
+myCar.adjustTemp();
+console.log(myCar.temp);
+
+
+assemblyLine.hasAudio(myCar);
+myCar.currentTrack = {
+    name: 'Never Gonna Give You Up',
+    artist: 'Rick Astley'
+};
+myCar.nowPlaying();
+
+assemblyLine.hasParktronic(myCar);
+myCar.checkDistance(0.4);
+myCar.checkDistance(0.2);
+console.log(myCar);
