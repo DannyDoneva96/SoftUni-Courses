bbfd7cde5cad5834eb5bfe7f1da587917504cc18
DannyDoneva96 <danielasinmas1@gmail.com>
2022-01-21 12:37:26 +0200

    lab exes

--- JavaScript-Advanced/DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources (1)/05.Colorize Table/ColorizeTable.js
+++ JavaScript-Advanced/DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources (1)/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,8 @@
 function colorize() {
-    // TODO
+    const rows = document.querySelectorAll('table tr');
+
+    for (let i = 1; i < rows.length; i+=2) {
+        rows[i].style.backgroundColor ='teal';
+
+    }
 }
--- JavaScript-Advanced/DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources (1)/06.Sum Table/SumTable.js
+++ JavaScript-Advanced/DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources (1)/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
 
+     let td= document.querySelectorAll('table tr td')
+      let array =Array.from(td)
+      let sum = 0;
+       for (let i=1; i<array.length; i+=2){
+            sum+=Number(array[i].textContent)
+       }
+
+        document.getElementById('sum').textContent = sum
 }
