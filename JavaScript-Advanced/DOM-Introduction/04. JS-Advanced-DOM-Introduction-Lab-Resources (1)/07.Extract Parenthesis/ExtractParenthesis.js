41c0a4a91f791ab70dfd9916169ef9f377203fa6
DannyDoneva96 <danielasinmas1@gmail.com>
2022-01-25 09:58:54 +0200

    Resources

--- JavaScript-Advanced/DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources (1)/07.Extract Parenthesis/ExtractParenthesis.js
+++ JavaScript-Advanced/DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources (1)/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,11 @@
 function extract(content) {
 
 }
+
+function binaryArrayToNumber(dec) {
+    let num = dec.join('');
+    num = Number(num);
+    let result =  (num >>> 0);
+  return result 
+  } 
+  console.log(binaryArrayToNumber([0,0,1,0]))
