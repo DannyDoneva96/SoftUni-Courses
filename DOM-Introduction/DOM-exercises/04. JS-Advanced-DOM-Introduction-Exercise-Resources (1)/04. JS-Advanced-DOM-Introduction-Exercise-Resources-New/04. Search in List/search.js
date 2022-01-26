4ce94f84fc53472d7b83d82fbc8702f46b67b1f5
DannyDoneva96 <danielasinmas1@gmail.com>
2022-01-26 15:01:42 +0200

    exes

--- DOM-Introduction/DOM-exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources (1)/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
+++ DOM-Introduction/DOM-exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources (1)/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/04. Search in List/search.js
@@ -1,4 +1,25 @@
+
 function search() {
-  let towns =  Array.from(document.querySelectorAll('#towns li')).textContent;
- console.log(towns)
+   //1. Select elements
+   let searchText = document.getElementById('searchText').value;
+   let allLiElements = Array.from(document.querySelectorAll('#towns li'));
+   //3. Main logic
+      //a. clear results from previous search
+      allLiElements.forEach(el => {
+         el.style.fontWeight = 'normal';
+         el.style.textDecoration = 'none';
+      });
+      //b. filter all li witch contain the search text
+      let filterLi = allLiElements
+         .filter(x => x.textContent.includes(searchText));
+      let mappedLi = filterLi.forEach(x => {
+         x.style.fontWeight = 'bold';
+         x.style.textDecoration = 'underline';
+      });
+      
+      //d. show number of matches in result div
+      let resultDiv = document.getElementById('result');
+      resultDiv.textContent = `${filterLi.length} matches found`;
+
+   
 }
--- DOM-Introduction/DOM-exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources (1)/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
+++ DOM-Introduction/DOM-exercises/04. JS-Advanced-DOM-Introduction-Exercise-Resources (1)/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/05. Table - Search Engine/solution.js
@@ -2,7 +2,19 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+      
+      let searchEl= document.getElementById('searchField').value
+      let rowEl = Array.from(document.querySelectorAll('tbody tr'))
 
+      rowEl.forEach(el=>{el.className="";});
+
+      let filtredRows=rowEl.filter(el=>{
+         let values = Array.from(el.children);
+         if(values.some(x=>x.textContent.includes(searchEl))){
+            el.className = 'select'
+         }
+      });
+      document.getElementById('searchField').value= '';
    }
+
 }
