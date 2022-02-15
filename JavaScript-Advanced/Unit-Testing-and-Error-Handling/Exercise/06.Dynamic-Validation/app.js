d1babd774339752218dce358a7c70c5a6fb5cc8b
DannyDoneva96 <danielasinmas1@gmail.com>
2022-02-15 11:12:58 +0200

    JS

--- JavaScript-Advanced/Unit-Testing-and-Error-Handling/Exercise/06.Dynamic-Validation/app.js
+++ JavaScript-Advanced/Unit-Testing-and-Error-Handling/Exercise/06.Dynamic-Validation/app.js
@@ -1,15 +1,41 @@
-function validate() {
-    function validate() {
-        const emailInput = document.getElementById('email');
-    
-        emailInput.addEventListener('change', () => {
-            const email = emailInput.value;
-            const regPattern = /^[a-z]+\@[a-z]+\.[a-z]+$/g;
-    
-            if(!regPattern.test(email)) {
-                emailInput.classList.add('error');
-            } else {
-                emailInput.classList.remove('error')
-            }
-        });
-    };}
+
+function validate(){
+
+let input = document.getElementById('email');
+
+let email = input.value;
+ input.addEventListener('change',onChange)
+
+ function onChange(){  
+      const regPattern = /^[a-z]+\@[a-z]+\.[a-z]+$/g;
+       if(!regPattern.test(email)){
+        input.classList.add('error');
+       }else{
+        input.classList.remove('error');
+       }
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
