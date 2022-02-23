27a4ac19dc7104b745b545f486968b387d6d77a1
DannyDoneva96 <danielasinmas1@gmail.com>
2022-02-23 14:11:53 +0200

    JS

--- DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources/03.Sum Numbers/SumNumbers.js
+++
@@ -1,9 +0,0 @@
-function calc() {
-
-    let num1 = document.getElementById("num1");
-    let num2 = document.getElementById("num2");
-    let result = document.getElementById("sum");
-    result.value = Number(num1.value) + Number(num2.value);
-
-
-}
--- DOM-Introduction/04. JS-Advanced-DOM-Introduction-Lab-Resources/04.Show More/ShowMore.js
+++
@@ -1,6 +0,0 @@
-    function showText() {
-        document.getElementById('text')
-            .style.display = 'inline';
-        document.getElementById('more')
-            .style.display = 'none';
-    }
