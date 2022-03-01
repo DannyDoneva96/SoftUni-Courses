64cc1f3358a79f791e27e58ff22857db441f873e
DannyDoneva96 <danielasinmas1@gmail.com>
2022-03-01 14:30:31 +0200

    JS

--- JS-Application/2.Asynchronous-Programming/Resources/02.Github-Repos/scripts/app.js
+++ JS-Application/2.Asynchronous-Programming/Resources/02.Github-Repos/scripts/app.js
@@ -1,3 +1,33 @@
 function loadRepos() {
-	console.log("TODO...");
+
+	let list = document.getElementById('repos');
+	let username = document.getElementById('username').value
+
+	let url = 'https://api.github.com/users/' + username + '/repos';
+
+	fetch(url)
+		.then(res => {
+			if (res.ok == false) {
+				throw new Error(`${res.status} ${res.statusText}`);
+			}
+			return res.json();
+		})
+		.then(handleResponse)
+		.catch(handleError);
+
+	function handleResponse(data) {
+		list.innerHTML = '';
+
+		for (let repo of data) {
+			let liEl = document.createElement('li');
+			liEl.innerHTML = `<a href="${repo.html_url}">${repo.full_name}</a>`;
+			list.appendChild(liEl);
+		}
+	}
+
+	function handleError(error) {
+		list.innerHTML = '';
+		list.textContent = `${error.message}`
+	}
+
 }
