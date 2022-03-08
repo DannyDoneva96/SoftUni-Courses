b0edc446aa9737b767e4234ee305c174a9a07466
DannyDoneva96 <danielasinmas1@gmail.com>
2022-03-08 19:23:31 +0200

    js

--- JS-Application/2.Asynchronous-Programming/Resources/03.Gighub-Commits/app.js
+++ JS-Application/2.Asynchronous-Programming/Resources/03.Gighub-Commits/app.js
@@ -1,4 +1,28 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let usernameInput = document.querySelector('#username');
+    let repoInput = document.querySelector('#repo');
+    let output = document.querySelector('#commits');
+
+    let url = `https://api.github.com/repos/${usernameInput.value}/${repoInput.value}/commits`;
+
+    fetch(url)
+        .then(res => {
+            if (res.status === 404) {
+                output.querySelectorAll('li').forEach(li => li.remove());
+                let li = document.createElement('li');
+                li.textContent = `Error: ${res.status} (Not Found)`;
+                output.appendChild(li);
+                throw new Error('not found');
+            } else {
+                return res.json();
+            }
+        })
+        .then(data => {
+            output.querySelectorAll('li').forEach(li => li.remove());
+            data.forEach(obj => {
+                let li = document.createElement('li');
+                li.textContent = `${obj.commit.author.name}: ${obj.commit.message}`;
+                output.appendChild(li);
+            });
+        });
 }
