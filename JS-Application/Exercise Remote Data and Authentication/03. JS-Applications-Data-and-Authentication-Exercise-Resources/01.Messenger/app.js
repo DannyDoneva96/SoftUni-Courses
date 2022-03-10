7597daae91c2f6da9709d3cce8ef275380468095
DannyDoneva96 <danielasinmas1@gmail.com>
2022-03-10 15:45:30 +0200

    requests

---
+++ JS-Application/Exercise Remote Data and Authentication/03. JS-Applications-Data-and-Authentication-Exercise-Resources/01.Messenger/app.js
@@ -0,0 +1,46 @@
+function attachEvents() {
+ document.getElementById('refresh').addEventListener('click',leadMessage);
+
+document.getElementById('submit').addEventListener('click',submitMessage);
+
+
+}
+attachEvents();
+let authorInp = document.querySelector('[name="author"]')
+let contentInp = document.querySelector('[name="content"]')
+let list = document.getElementById('messages');
+
+
+async function submitMessage(){
+    let author = authorInp.value;
+    let content =contentInp.value;
+    let result = await createMessage({author,content})
+    contentInp.value=''
+    // list.value+='/n' + `${author}:${content}`;
+
+}
+
+async function leadMessage() {
+    let url = 'http://localhost:3030/jsonstore/messenger';
+
+    let res = await fetch(url);
+    let data = await res.json();
+     let messages = Object.values(data)
+     let list = document.getElementById('messages');
+
+     list.value = messages.map(message =>`${message.author}: ${message.content}`).join('\n');
+}
+async function createMessage(message){
+    let url = 'http://localhost:3030/jsonstore/messenger';
+    let options = {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(message)
+    };
+    let res = await fetch(url, options);
+    let data = await res.json();
+    return data;
+
+}
