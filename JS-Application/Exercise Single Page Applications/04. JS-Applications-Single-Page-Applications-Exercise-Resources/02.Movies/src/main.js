3941a449d033bbb8d8af73385bb99f17ae409919
DannyDoneva96 <danielasinmas1@gmail.com>
2022-03-25 10:02:45 +0200

    JS

---
+++ JS-Application/Exercise Single Page Applications/04. JS-Applications-Single-Page-Applications-Exercise-Resources/02.Movies/src/main.js
@@ -0,0 +1,15 @@
+// create modules for every view    
+// configurate navigation with eventlisteners
+//implent modules  
+//create async functions for request  
+// implement dom logic
+
+
+// order of views:
+// - catakog 
+//- login/reg
+//- create 
+// - details
+// likes
+// edit 
+// delete 
