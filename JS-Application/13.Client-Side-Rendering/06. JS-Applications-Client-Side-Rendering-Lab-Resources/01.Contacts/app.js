c48ae22ec92be98bd6b95dd300d3ee7eebe8c5ae
DannyDoneva96 <danielasinmas1@gmail.com>
2022-03-26 10:08:48 +0200

    JS

---
+++ JS-Application/13.Client-Side-Rendering/06. JS-Applications-Client-Side-Rendering-Lab-Resources/01.Contacts/app.js
@@ -0,0 +1,41 @@
+import {contacts} from './contacts.js';
+import {html,render} from './node_modules/lit-html/lit-html.js';
+
+
+let contactTemplate =(data)=> html` <div class="contact card">
+<div>
+    <i class="far fa-user-circle gravatar"></i>
+</div>
+<div class="info">
+    <h2>Name: ${data.name}</h2>
+    <button class="detailsBtn">Details</button>
+    <div class="details" id=${data.id}>
+        <p>Phone number: ${data.phoneNumber}</p>
+        <p>Email: ${data.email}</p>
+    </div>
+</div>`
+
+start()
+
+
+
+function start() {
+    let container = document.getElementById('contacts')
+    render(contacts.map(contactTemplate),container);
+}
+
+let colection =document.getElementsByClassName('detailsBtn')
+  for(let i = 0; i < colection.length; i++) {
+
+    colection[i].addEventListener('click', onClick)
+}
+
+function onClick(e) {
+  
+   let div =  e.target.parentNode.parentNode
+   let needenEl= div.getElementsByClassName('details')
+
+    needenEl[0].style.display="none" ? 'block':'none'
+    needenEl[0].style.display="none" ? 'block':'none'
+
+}
